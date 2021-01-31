bb68f76f4ac1eb53893757a6872b6f141919ae3c
Dipesh Chaudhari <chaudhari.dipesh3360@gmail.com>
2021-01-31 20:14:12 +0530

    React Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Nav from './Nav';
+import About from './About';
+import Shop from './Shop';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav/>
+      <Switch>
+       {/* <Route path="/" component={App}/>  */}
+      <Route path="/about" component={About}/>
+      
+      <Route path="/shop" component={Shop}/>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
---
+++ src/Shop.js
@@ -0,0 +1,26 @@
+import React, { useEffect}from 'react';
+import './App.css';
+
+
+function Shop() {
+
+    useEffect(() => {
+        fetchItems();
+
+    },[]);
+
+    const fetchItems = async () => {
+        const data = await fetch('https://fortnite-public-api.theapinetwork.com/prod09/upcoming/get');
+        const items = await data.json();
+        console.log(items);
+    };
+
+
+    return (
+        <div>
+            <h1>Shop page</h1>
+        </div>
+    )
+}
+
+export default Shop;
